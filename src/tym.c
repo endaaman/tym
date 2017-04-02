43f99a5b4c72f3251527b0974526165c9d1d2791
endaaman <endaaman@eis.hokudai.ac.jp>
2017-04-03 08:00:30 +0900

    refactor

--- src/tym.c
+++ src/tym.c
@@ -1,26 +1,11 @@
-/*
-The MIT License (MIT)
-
-Copyright (c) 2017 endaaman.
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
+/**
+ * tym.c
+ *
+ * Copyright (c) 2017 endaaman
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
 
 #include <stdlib.h>
 #include <stdbool.h>
@@ -379,8 +364,8 @@ int main(int argc, char* argv[])
 
   GHashTable *config = config_init();
   config_load(config);
-  gtk_init(&argc, &argv);
 
+  gtk_init(&argc, &argv);
   start(config);
 
   config_close(config);
