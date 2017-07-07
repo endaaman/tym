96790e1298f5e9f1b4b77524cd68979489ba9850
endaaman <buhibuhidog@gmail.com>
2017-07-08 03:51:12 +0900

    update banner

--- src/tym.h
+++ src/tym.h
@@ -4,7 +4,7 @@
  * Copyright (c) 2017 endaaman
  *
  * This software may be modified and distributed under the terms
- * of the MIT license.  See the LICENSE file for details.
+ * of the MIT license. See the LICENSE file for details.
  */
 
 #ifndef TYM_H
