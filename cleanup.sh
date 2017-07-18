74038d013bf51b03b69ba07f040356ffd6a49a02
endaaman <buhibuhidog@gmail.com>
2017-07-19 03:02:41 +0900

    update script

--- cleanup.sh
+++ cleanup.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-rm -f Makefile
-rm -f Makefile.in
-rm -f aclocal.m4
-rm -f compile
-rm -f config.log
-rm -f config.status
-rm -f configure
-rm -f depcomp
-rm -f include/Makefile
-rm -f include/Makefile.in
-rm -f install-sh
-rm -f libtool
-rm -f missing
-rm -f src/Makefile
-rm -f src/Makefile.in
-rm -f src/toollib/*.la
-rm -f src/toollib/*.lo
-rm -f src/toollib/.dirstamp
-rm -f stamp-h1
+rm -rf Makefile
+rm -rf Makefile.in
+rm -rf aclocal.m4
 rm -rf autom4te.cache/
 rm -rf aux-dist/
+rm -rf compile
+rm -rf config.log
+rm -rf config.status
+rm -rf configure
+rm -rf depcomp
+rm -rf include/Makefile
+rm -rf include/Makefile.in
+rm -rf install-sh
+rm -rf libtool
 rm -rf m4/
+rm -rf missing
 rm -rf src/*.la
 rm -rf src/*.lo
 rm -rf src/.deps
 rm -rf src/.libs
+rm -rf src/Makefile
+rm -rf src/Makefile.in
 rm -rf src/libtool
+rm -rf src/toollib/*.la
+rm -rf src/toollib/*.lo
 rm -rf src/toollib/.deps
+rm -rf src/toollib/.dirstamp
+rm -rf stamp-h1
 
 rm -rf src/version.h
 
