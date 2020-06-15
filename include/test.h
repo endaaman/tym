55a84a77f14b1fd8182fab703d4d5161d6ff26ca
endaaman <buhibuhidog@gmail.com>
2020-06-16 03:18:12 +0900

    split files

---
+++ include/test.h
@@ -0,0 +1,17 @@
+/**
+ * tym.h
+ *
+ * Copyright (c) 2020 endaaman
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+#ifndef TEST_H
+#define TEST_H
+
+#include "context.h"
+
+void test_regex();
+
+#endif
