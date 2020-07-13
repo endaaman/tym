2350465f12aee6119c31bb2ea1e8317215ab71c1
endaaman <buhibuhidog@gmail.com>
2020-07-13 14:48:14 +0900

    update script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -5,12 +5,16 @@ set -eux
 rm -f Makefile
 rm -f Makefile.in
 rm -f aclocal.m4
+rm -f app-config.h
+rm -f app-config.h.in
 rm -f compile
+rm -f config.guess
 rm -f config.h
 rm -f config.h.in
 rm -f config.h.in~
 rm -f config.log
 rm -f config.status
+rm -f config.sub
 rm -f configure
 rm -f depcomp
 rm -f install-sh
@@ -20,19 +24,21 @@ rm -f stamp-h1
 rm -rf autom4te.cache/
 rm -rf aux-dist/
 rm -rf m4/
+
 rm -f src/*.la
 rm -f src/*.lo
+rm -f src/*.log
 rm -f src/*.o
+rm -f src/*.trs
 rm -f src/Makefile
 rm -f src/Makefile.in
 rm -f src/tym
+rm -f src/tym-test
 rm -rf src/.deps/
 
-rm -f src/common.h
-
-rm -f src/*.log
-rm -f src/*.trs
-rm -f src/tym-test
+rm -f include/Makefile
+rm -f include/Makefile.in
+rm -f include/common.h
 
 rm -f tym.1
 rm -f test-driver
